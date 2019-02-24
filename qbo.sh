f9312872be36c763be06c23deb3c9bbddf011884
Dominic Spatz <sptzdmnc@gmail.com>
2019-02-24 10:09:03 +0100

    Updated script
    Dynamic hosts, uses recipe.json (included two examples)

--- qbo.sh
+++ qbo.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 # Inspired by: https://github.com/michaelhenningersrb/home-assistant-config/wiki/Reingeneered-API-for-the-QBO-machine
 
-Proto="https://"
-Host="qbo"
 curl_settings="curl -s -k -H Content-Type: application/json"
-uid='00000000-0000-0000-0000-000000000000'
-creator='22'
-user='33'
-username='smarthome'
+prefix="https://"
+path="/etc/openhab2/scripts/qbo"
+qloud_url="https://qloud.qbo.coffee"
 
 # Get status
-if [ $# -eq 1 ] && [ "$1" = "status" ]
+if [ $# -eq 2 ] && [ "$2" = "status" ]
 then
-	$curl_settings -X GET $Proto$Host/status/maintenance
+	$curl_settings -X GET $prefix$1/status/maintenance
+# Get machineinfo
+elif [ $# -eq 2 ] && [ "$2" = "machineinfo" ]
+then
+	$curl_settings -X GET $prefix$1/machineInfo
+# Get firmwareinfo
+elif [ $# -eq 1 ] && [ "$1" = "fw" ]
+then
+	$curl_settings -X GET $qloud_url/firmware/latest
 # Get recipes
-elif [ $# -eq 1 ] && [ "$1" = "recipes" ]
+elif [ $# -eq 2 ] && [ "$2" = "recipes" ]
 then
-	$curl_settings -X GET $Proto$Host/recipes
-# Espresso 70ml
-elif [ $# -eq 1 ] && [ "$1" = "1" ]
+	$curl_settings -X GET $prefix$1/recipes
+# post JSON file to machine
+elif [ $# -eq 2 ] && [ -f $path/$2.json ]
 then
-	$curl_settings -d ' {"uid": "'$uid'","creatorId": "'$creator'","userId": "'$user'","userName": "'$username'","name": "Espresso","coffeeAmount": 70,"foamAmount": 0,"milkAmount": 0,"cupSize": 80,"creationTimestamp": 15,"defaultRecipe": 1,"usesHotMilk": 1,"favouriteCapsuleId": -1,"sequence": {  "index_0": 2,  "index_1": 3,  "index_2": 4,  "index_3": 0,  "index_4": 0  }}' -X POST $Proto$Host/brew
-# Kaffee 170ml
-elif [ $# -eq 1 ] && [ "$1" = "2" ]
-then
-	$curl_settings -d ' {"uid": "'$uid'","creatorId": "'$creator'","userId": "'$user'","userName": "'$username'","name": "Kaffee","coffeeAmount": 170,"foamAmount": 0,"milkAmount": 0,"cupSize": 200,"creationTimestamp": 15,"defaultRecipe": 1,"usesHotMilk": 1,"favouriteCapsuleId": -1,"sequence": {  "index_0": 2,  "index_1": 3,  "index_2": 4,  "index_3": 0,  "index_4": 0  }}' -X POST $Proto$Host/brew
+	$curl_settings -d @"$path/$2.json" $prefix$1/brew
 else
 	echo error
 fi
+
+
